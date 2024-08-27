05a5f63dc97f37083e5666133db61b2ee24df1a4
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2024-08-27 12:31:14 -0400

    main page banner complete

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Stick+No+Bills:wght@200..800&display=swap" rel="stylesheet"  />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="https://fonts.googleapis.com/css2?family=Underdog&display=swap" rel="stylesheet"></link>
       </head>
       <div className="App">
         <Navbar />
--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -6,7 +6,27 @@ class Banner extends React.Component {
 
     render() {
         return (
-            <div className="banner"></div>
+            <div className="banner-header">
+                <div className="lowest">
+                    <img alt="bglow" src={process.env.PUBLIC_URL + "/images/bg/istanbul.jpg"} />
+                </div>
+                <div className="middle">
+                    <img alt="bgmid" src={process.env.PUBLIC_URL + "/images/bg/space.jpg"} />
+                </div>
+                <div className="filter" />
+                <div className="highest">
+                    <div className="decor-container-first">
+                        <h1 className="text-first underdog-regular">Faruk Efe</h1>
+                        <div className="decoration" />
+                    </div>
+                    <div className="decor-container-second">
+                        <h1 className="text-last underdog-regular">Yencilek</h1>
+                        <div className="decoration" />
+                    </div>
+                    <div className="banner-img-left"><img alt="img1" className="left-img" src={process.env.PUBLIC_URL + "/images/banner1.png"} style={{width: 350, height: 350}} /></div>
+                    <div className="banner-img-right"><img alt="img2" className="right-img" src={process.env.PUBLIC_URL + "/images/banner2.png"} style={{width: 350, height: 350}} /></div>
+                </div>
+            </div>
         )
     }
 }
