dec93471ddf3ea624c210148a797edbdc64d19bd
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2024-09-08 16:33:19 -0400

    Entry cards functioning as intended

--- src/Components/Entry/index.js
+++ src/Components/Entry/index.js
@@ -6,7 +6,29 @@ class Entry extends React.Component {
 
     get_description() {
         // This function will go thru every single item in dictionary to get entry list items and display them
-        return <li></li>
+        let d_list = this.props.entry.description;
+        let list_items = [];
+        for (let i=0;i<d_list.length;i++) {
+            let item = d_list[i];
+            let tag = <li className="entry-desc-item stick-medium">{item}</li>;
+            list_items.push(tag);
+        }
+        return <ul>
+            {list_items}
+        </ul>;
+    }
+
+    get_tags() {
+        // Gets tags for a given experience for display
+        let tag_list = this.props.entry.tags;
+        let html_tags = [];
+        for (let i=0;i<tag_list.length;i++) {
+            let data = tag_list[i];
+            let html_item = <span className="entry-tags">{data}</span>;
+            html_tags.push(html_item);
+        }
+
+        return html_tags;
     }
 
     render() {
@@ -15,15 +37,18 @@ class Entry extends React.Component {
                 <div className="entry-info">
                     <div className="title-row">
                         <div className="push-left">
-                            <h1 className="entry-title">{this.props.entry.title}</h1>
+                            <h1 className="entry-title stick-semibold">{this.props.entry.title}</h1>
                             <a className="entry-source" href={this.props.entry.url}><img alt="" src={process.env.PUBLIC_URL + "/images/socials/" + this.props.entry.logo} style={{width: 25, height: 25}} /></a>
                         </div>
                         <div className="push-right">
-                            <h2 className="period">{this.props.entry.title}</h2>
+                            <h2 className="entry-period stick-medium">{this.props.entry.title}</h2>
                         </div>
                     </div>
+                    <div className="tags">
+                        {this.get_tags()}
+                    </div>
                     <div className="entry-items">
-                        <ul>{this.get_description()}</ul>
+                        {this.get_description()}
                     </div>
                 </div>
                 <div className="entry-filter" />
--- src/Pages/Experiences/index.js
+++ src/Pages/Experiences/index.js
@@ -4,10 +4,15 @@ import Entry from "../../Components/Entry";
 import "./styles.css";
 
 let sample_entry = {
+    background: "binary-bg.jpg",
     title: "Hahaha",
     logo: "github.svg",
-    link: "/",
-    background: "binary-bg.jpg"
+    tags: ["CNN","Tensorflow","Python"],
+    url: "/",
+    description: [
+        "Developed an Convolutional Neural Network Model that classifies the brain tumor into categories from the MRI image.",
+        "Developed an Convolutional Neural Network Model that classifies the brain tumor into categories from the MRI image."
+    ]
 }
 
 export default function Experiences(props) {
